dd1096c9758056860d5ed334de2314063ecf73a0
Muchen He <i@mansurhe.com>
2017-07-26 09:14:11 -0700

    Changed function class to actual class

--- sketch.js
+++ sketch.js
@@ -15,9 +15,36 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 var GRID_SIZE = 20;
-var BORDER_SIZE = 50;
+var BORDER_SIZE = 0;
 
 var lines = [];
+
+// ======================= Classes =====================
+class SinglePort {
+    constructor(x, y) {
+        this.x1 = x;
+        this.y1 = y;
+        this.x2 = 0;
+        this.y2 = 0;
+        this.built =  false;
+    }
+
+    draw() {
+        if (this.built) line(this.x1, this.y1, this.x2, this.y2);
+        else line(
+            this.x1,
+            this.y1,
+            round(mouseX / GRID_SIZE) * GRID_SIZE,
+            round(mouseY / GRID_SIZE) * GRID_SIZE
+        )
+    }
+
+    setEnd(x, y) {
+        this.x2 = x;
+        this.y2 = y;
+        this.built = true;
+    }
+}
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
@@ -27,6 +54,7 @@ function setup() {
 function draw() {
     background(255);
 
+    drawCursor();
     drawGrid();
 
     for (var i = 0; i < lines.length; i++) {
@@ -43,31 +71,25 @@ function drawGrid() {
     }
 }
 
+function drawCursor() {
+    ellipse(
+        round(mouseX / GRID_SIZE) * GRID_SIZE, 
+        round(mouseY / GRID_SIZE) * GRID_SIZE, 
+        5, 
+        5
+    );
+}
+
 function mousePressed() {
-    lines.push(new Line(mouseX, mouseY));
+    lines.push(new SinglePort(
+        round(mouseX / GRID_SIZE) * GRID_SIZE, 
+        round(mouseY / GRID_SIZE) * GRID_SIZE
+    ));
 }
 
 function mouseReleased() {
-    lines[lines.length - 1].setEnd(mouseX, mouseY);
-}
-
-function Line(x, y) {
-    this.x1 = x;
-    this.y1 = y;
-
-    this.x2 = 0;
-    this.y2 = 0;
-
-    this.built = false;
-
-    this.draw = function() {
-        if (this.built) line(this.x1, this.y1, this.x2, this.y2);
-        else line(this.x1, this.y1, mouseX, mouseY);
-    }
-
-    this.setEnd = function(x, y) {
-        this.x2 = x;
-        this.y2 = y;
-        this.built = true;
-    }
+    lines[lines.length - 1].setEnd(
+        round(mouseX / GRID_SIZE) * GRID_SIZE, 
+        round(mouseY / GRID_SIZE) * GRID_SIZE
+    );
 }
