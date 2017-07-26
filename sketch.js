582d92679bff77abefed12a49facaca9c4381131
Muchen He <i@mansurhe.com>
2017-07-25 22:17:56 -0700

    Added license

--- sketch.js
+++ sketch.js
@@ -1,3 +1,19 @@
+// FSXAC/CircuitDraw: Javascript webapp to draw circuit diagrams
+// Copyright (C) 2017 Muchen He
+
+// FSXAC/CircuitDraw is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// FSXAC/CircuitDraw is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 var GRID_SIZE = 20;
 var BORDER_SIZE = 50;
 
